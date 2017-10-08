3d584ee775daa592a7b17c35209318e8c0441516
Krzysztof Biegański <kbieganski95@gmail.com>
2017-10-08 18:26:16 +0200

    Added loading normals from OBJ files

--- engine/src/Mesh.cpp
+++ engine/src/Mesh.cpp
@@ -16,6 +16,7 @@ using std::unordered_map;
 
 struct Vertex {
 	vec3 position;
+	vec3 normal;
 	vec2 textureCoordinates;
 
 	bool operator==(const Vertex& other) const {
@@ -27,7 +28,9 @@ struct Vertex {
 namespace std {
 	template<> struct hash<Vertex> {
 		size_t operator()(const Vertex& vertex) const {
-			return hash<vec3>()(vertex.position) ^ (hash<vec2>()(vertex.textureCoordinates) << 1);
+			return (hash<vec3>()(vertex.position)
+				 ^ (hash<vec3>()(vertex.normal) << 1) >> 1)
+				 ^ (hash<vec2>()(vertex.textureCoordinates) << 1);
 		}
 	};
 }
@@ -42,8 +45,8 @@ Mesh::Mesh(shared_ptr<const GraphicsContext> context, const string& filename) {
 
 void Mesh::createAttributeBindings(RenderPipelineBuilder& pipelineBuilder) const {
 	pipelineBuilder.createAttributeBinding(sizeof(Vertex),
-										   { VK_FORMAT_R32G32B32_SFLOAT, VK_FORMAT_R32G32_SFLOAT },
-										   { offsetof(Vertex, position), offsetof(Vertex, textureCoordinates) });
+										   { VK_FORMAT_R32G32B32_SFLOAT, VK_FORMAT_R32G32B32_SFLOAT, VK_FORMAT_R32G32_SFLOAT },
+										   { offsetof(Vertex, position), offsetof(Vertex, normal), offsetof(Vertex, textureCoordinates) });
 }
 
 
@@ -69,12 +72,16 @@ void Mesh::createBuffers(shared_ptr<const GraphicsContext> context, tinyobj::att
 	vector<Vertex> vertices;
 	vector<uint32_t> indices;
 	unordered_map<Vertex, uint32_t> uniqueVertices;
+	ASSERT(!attrib.normals.empty(), "Failed to load mesh - it does not define vertex normals");
 	for (auto& shape : shapes) {
 		for (auto& index : shape.mesh.indices) {
 			Vertex vertex;
 			vertex.position = { attrib.vertices[3 * index.vertex_index + 0],
 								attrib.vertices[3 * index.vertex_index + 1],
 								attrib.vertices[3 * index.vertex_index + 2] };
+			vertex.normal = { attrib.normals[3 * index.normal_index + 0],
+							  attrib.normals[3 * index.normal_index + 1],
+							  attrib.normals[3 * index.normal_index + 2] };
 			vertex.textureCoordinates = { attrib.texcoords[2 * index.texcoord_index + 0],
 										  1 - attrib.texcoords[2 * index.texcoord_index + 1] };
 			if (uniqueVertices.find(vertex) == uniqueVertices.end()) {
